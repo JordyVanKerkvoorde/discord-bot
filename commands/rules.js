689ff38535a9809436537d588e69d958e9d22a0f
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-17 21:00:04 +0100

    Add bot activity

--- commands/rules.js
+++ commands/rules.js
@@ -19,7 +19,7 @@ module.exports = {
         return message.author.send(data, { split: true })
             .then(() => {
                 if (message.channel.type === 'dm') return;
-                message.reply('I\'ve sent you a DM with all my commands!');
+                message.reply('I\'ve sent you a DM with all the rules!');
             })
             .catch(error => {
                 console.error(`Could not send help DM to ${message.author.tag}.\n`, error);

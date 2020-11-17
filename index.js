4f37bcc8035bc7c547850939d3a1cc0ce6edcbf0
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-17 17:56:25 +0100

    Add argument test

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-require('dotenv').config()
-const Discord = require('discord.js')
+require('dotenv').config();
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const {prefix} = require('./config.json');
+const data = require('./data.json')
 
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
   
-  client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
+  client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === `ping`) {
+      message.reply('pong');
+      if(args[0] === 'pong'){
+          return message.channel.send('ping')
+      }
     }
   });
   

e67b70ede6f4c3e52b29c2c864fb7e40da665ceb
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-17 18:30:19 +0100

    Add command separation + checks

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
+const fs = require('fs')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const {prefix} = require('./config.json');
 const data = require('./data.json')
 
+client.commands = new Discord.Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -13,13 +21,28 @@ client.on('ready', () => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName);
 
-    if (command === `ping`) {
-      message.reply('pong');
-      if(args[0] === 'pong'){
-          return message.channel.send('ping')
-      }
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+        return message.channel.send(reply);
+    }
+
+    if (!client.commands.has(commandName)) return;
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     }
   });
   

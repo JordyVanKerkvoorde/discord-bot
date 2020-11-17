2b625eca3b42270363c84e0eb6f1724aa38f9799
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-17 19:54:58 +0100

    Add rules command

---
+++ commands/rules.js
@@ -0,0 +1,29 @@
+const { rules } = require('../data.json');
+
+module.exports = {
+    name: 'rules',
+    aliases: ['rule'],
+	description: 'List all of all the server rules',
+	cooldown: 5,
+	execute(message, args) {
+        const data = [];
+
+        rules.forEach(rule =>{
+            data.push(`**${rule.name}**`);
+            rule.text.forEach(line => {
+                data.push(`${line}`);
+            });
+            data.push('');
+        });
+
+        return message.author.send(data, { split: true })
+            .then(() => {
+                if (message.channel.type === 'dm') return;
+                message.reply('I\'ve sent you a DM with all my commands!');
+            })
+            .catch(error => {
+                console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+            });
+    }
+};
